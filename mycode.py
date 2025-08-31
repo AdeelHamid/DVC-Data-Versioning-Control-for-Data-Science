66436174f07db2da9b038e5736ab494919b46613
Adeel Hamid <adeel.hamid50@gmail.com>
2025-08-31 23:46:13 +1000

    second version of data is saved

--- mycode.py
+++ mycode.py
@@ -8,6 +8,11 @@ data = { 'Name': ['Alice', 'Bob', 'Charlie'],
 
 df = pd.DataFrame(data)
 
+# Adding a new record into dataFrame V2
+
+new_record = {'Name': 'GF1', 'Age': 28, 'City': 'San Francisco'}
+df.loc[len(df.index)] = new_record
+
 # Ensure the "data" directory is in root
 data_dir = 'data'
 os.makedirs(data_dir, exist_ok=True)
